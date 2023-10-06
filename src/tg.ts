17ce9325e7da3d0d8bb2fd4719a1aa655887f43f
Myphz <danielkeq@gmail.com>
2023-10-06 19:04:10 +0200

    fix: build error

--- src/tg.ts
+++ src/tg.ts
@@ -1,6 +1,6 @@
-import { getFile } from "./telegram";
+import { getFileLink } from "./telegram";
 
 (async () => {
   const a = "BQACAgQAAx0EZIpFjgADCmT7PlviprN4dlal0uC-HJVyusbSAAKREwAC_krZUy55bhLqxivXMAQ";
-  console.log(await getFile(a));
+  console.log(await getFileLink(a));
 })();
