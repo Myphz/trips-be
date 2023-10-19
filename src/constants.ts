f8ded5e15e36b6be7b30f801216104324fbcd3e3
Myphz <danielkeq@gmail.com>
2023-10-19 18:48:04 +0200

    chore: add default pexels api key

--- src/constants.ts
+++ src/constants.ts
@@ -9,4 +9,5 @@ export const fastify = Fastify({ logger: true });
 
 export const TRIPS_BOT_API_TOKEN = process.env.TRIPS_BOT_API_TOKEN ?? throwError("Telegram API Token not set!");
 export const CHAT_ID = process.env.CHAT_ID ?? throwError("CHAT_ID not set!");
-export const PEXELS_API_KEY = process.env.PEXELS_API_KEY ?? throwError("PEXELS_API_KEY not set!");
+export const PEXELS_API_KEY =
+  process.env.PEXELS_API_KEY ?? process.env.PEXELS_API_KEY_DEBUG ?? throwError("PEXELS_API_KEY not set!");
